7b4368cec37d9c850a9d4af057d61eb24b512cd9
Fishappy0 <fishappy0@gmail.com>
2022-07-19 20:06:36 +0700

    cleaning up tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,15 @@
+static config: &'static str = include_str!("../config.json");
 // use prclient::prclient;
 use pigrabbit::{types::Keys, *};
 use serde_json;
 use std::fs;
 use types::*;
+
+fn init_prclient() -> PRClient {
+    let key_struct = serde_json::from_str(&config).unwrap();
+    let mut prclient = pigrabbit::PRClient::new(key_struct);
+    prclient
+}
 
 fn block_on<T: std::future::Future>(s: T) -> T::Output {
     let rt = tokio::runtime::Runtime::new().unwrap();
@@ -10,46 +17,39 @@ fn block_on<T: std::future::Future>(s: T) -> T::Output {
 }
 #[test]
 fn retreive_ssl_certs() {
-    let config_file = fs::read_to_string("config.json").expect("[pigrabbit] file does not exist!");
-    let key_struct = serde_json::from_str(&config_file).unwrap();
-    let mut prclient = pigrabbit::PRClient::new(key_struct);
-
-    // let record = pigrabbit::types::record {
-    //     domain = "arvinderd.com"
-    // };
-    // print!("{:?}", key_struct);
+    let mut prclient = init_prclient();
     block_on(async {
-        let result = prclient
-            .retreive_ssl_by_domain(
-                // "a".to_owned(),
-                "arvinderd.com",
-                // "", // none,
-            )
+        prclient
+            .retreive_ssl_by_domain("arvinderd.com")
             .await
             .unwrap();
     });
-    // println!("{:?}", result);
 }
 
 #[test]
 fn retreive_by_domain_with_id() {
-    let config_file = fs::read_to_string("config.json").expect("[pigrabbit] file does not exist!");
-    let key_struct = serde_json::from_str(&config_file).unwrap();
-    let mut prclient = pigrabbit::PRClient::new(key_struct);
-
-    // let record = pigrabbit::types::record {
-    //     domain = "arvinderd.com"
-    // };
-    // print!("{:?}", key_struct);
+    let mut prclient = init_prclient();
     block_on(async {
-        let result = prclient
+        prclient
             .retreive_by_domain_with_id(
-                // "a".to_owned(),
                 "arvinderd.com",
                 "", // "", // none,
             )
             .await
             .unwrap();
     });
-    // println!("{:?}", result);
+}
+
+#[test]
+fn add_record() {
+    let mut prclient = init_prclient();
+    block_on(async {
+        prclient
+            .retreive_by_domain_with_id(
+                "arvinderd.com",
+                "", // "", // none,
+            )
+            .await
+            .unwrap();
+    });
 }
